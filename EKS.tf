b3c7b1b64fe9f65fe0b9d1f5702444befbab5367
borja <borja.ortizllamas1@gmail.com>
2020-05-30 16:37:39 +0200

    rol - diferent

--- EKS.tf
+++ EKS.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "CLUSTER" {
   name     = "UNIR-API-REST-CLUSTER"
-  role_arn = "${aws_iam_role.cluster-role.arn}"
+  role_arn = "${aws_iam_role.eks_cluster_role.arn}"
 
   vpc_config {
     subnet_ids = ["${aws_subnet.unir_subnet_cluster_2.id}", "${aws_subnet.unir_subnet_cluster_1.id}"]
