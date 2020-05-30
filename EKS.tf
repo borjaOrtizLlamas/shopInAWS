517fec8d3a78a0f2825ed1ce260e1a20efb0f642
borja <borja.ortizllamas1@gmail.com>
2020-05-30 21:41:02 +0200

    cluster

--- EKS.tf
+++ EKS.tf
@@ -1,21 +1,21 @@
-resource "aws_eks_cluster" "CLUSTER" {
-  name     = "UNIR-API-REST-CLUSTER-${var.SUFIX}"
-  role_arn = "${aws_iam_role.eks_cluster_role.arn}"
+#resource "aws_eks_cluster" "CLUSTER" {
+#  name     = "UNIR-API-REST-CLUSTER-${var.SUFIX}"
+#  role_arn = "${aws_iam_role.eks_cluster_role.arn}"
+#
+#  vpc_config {
+#    subnet_ids = [
+#      "${aws_subnet.unir_subnet_cluster_2.id}",
+#      "${aws_subnet.unir_subnet_cluster_1.id}"
+#    ]
+#  }
+#}
 
-  vpc_config {
-    subnet_ids = [
-      "${aws_subnet.unir_subnet_cluster_2.id}",
-      "${aws_subnet.unir_subnet_cluster_1.id}"
-    ]
-  }
-}
 
 
+#output "endpoint" {
+#  value = "${aws_eks_cluster.CLUSTER.endpoint}"
+#}
 
-output "endpoint" {
-  value = "${aws_eks_cluster.CLUSTER.endpoint}"
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = "${aws_eks_cluster.CLUSTER.certificate_authority.0.data}"
-}
+#output "kubeconfig-certificate-authority-data" {
+#  value = "${aws_eks_cluster.CLUSTER.certificate_authority.0.data}"
+#}
