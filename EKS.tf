8a7e96393d95ec0a5120e3e37f3029bc15e1d858
borja <borja.ortizllamas1@gmail.com>
2020-05-30 20:23:35 +0200

    rol - diferent

--- EKS.tf
+++ EKS.tf
@@ -7,24 +7,6 @@ resource "aws_eks_cluster" "CLUSTER" {
   }
 }
 
-resource "aws_eks_node_group" "eks_node" {
-  cluster_name    = "${aws_eks_cluster.CLUSTER.name}"
-  node_group_name = "shop_node_groups_${var.SUFIX}"
-  node_role_arn   ="${aws_iam_role.eks_nodes_role.arn}"
-  subnet_ids      = ["${aws_subnet.unir_subnet_cluster_2.id}", "${aws_subnet.unir_subnet_cluster_1.id}"]
-  
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
-
-  depends_on = [
-    "aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy",
-    "aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy",
-    "aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly"
-  ]
-}
 
 
 output "endpoint" {
