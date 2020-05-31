b4253ad95eaf63ebfbb00dbe1fb9d0e28f1140f9
borja <borja.ortizllamas1@gmail.com>
2020-05-31 04:25:38 +0200

    eks

--- EKS.tf
+++ EKS.tf
@@ -1,21 +1,45 @@
-#resource "aws_eks_cluster" "CLUSTER" {
-#  name     = "UNIR-API-REST-CLUSTER-${var.SUFIX}"
-#  role_arn = "${aws_iam_role.eks_cluster_role.arn}"
-#
-#  vpc_config {
-#    subnet_ids = [
-#      "${aws_subnet.unir_subnet_cluster_2.id}",
-#      "${aws_subnet.unir_subnet_cluster_1.id}"
-#    ]
-#  }
-#}
+resource "aws_eks_cluster" "CLUSTER" {
+  name     = "UNIR-API-REST-CLUSTER-${var.SUFIX}"
+  role_arn = "${aws_iam_role.eks_cluster_role.arn}"
+
+  vpc_config {
+    subnet_ids = [
+      "${aws_subnet.unir_subnet_cluster_1.id}","${aws_subnet.unir_subnet_cluster_2.id}",
+      "${aws_subnet.unir_subnet_cluster_3.id}","${aws_subnet.unir_subnet_cluster_4.id}",
+      "${aws_subnet.unir_subnet_cluster_5.id}","${aws_subnet.unir_subnet_cluster_6.id}"
+
+    ]
+  }
+}
 
 
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = "${aws_eks_cluster.CLUSTER.name}"
+  node_group_name = "node_sping_boot"
+  node_role_arn   = "${aws_iam_role.eks_nodes_role.arn}"
+  subnet_ids      = [
+    "${aws_subnet.unir_subnet_cluster_1.id}",
+    "${aws_subnet.unir_subnet_cluster_2.id}",      
+  ]
 
-#output "endpoint" {
-#  value = "${aws_eks_cluster.CLUSTER.endpoint}"
-#}
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
 
-#output "kubeconfig-certificate-authority-data" {
-#  value = "${aws_eks_cluster.CLUSTER.certificate_authority.0.data}"
-#}
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    "aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy",
+    "aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy",
+    "aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly",
+  ]
+}
+
+output "endpoint" {
+  value = "${aws_eks_cluster.CLUSTER.endpoint}"
+}
+output "kubeconfig-certificate-authority-data" {
+  value = "${aws_eks_cluster.CLUSTER.certificate_authority.0.data}"
+}
