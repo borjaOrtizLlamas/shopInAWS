75a58b6b7b6214b6486be0a17c648db2a3f1bd8f
borja <borja.ortizllamas1@gmail.com>
2020-06-01 23:58:11 +0200

    node

--- EKS.tf
+++ EKS.tf
@@ -6,35 +6,30 @@ resource "aws_eks_cluster" "CLUSTER" {
       "${aws_subnet.unir_subnet_cluster_1.id}","${aws_subnet.unir_subnet_cluster_2.id}"
     ]
   }
-  depends_on = [
-    "aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy",
-    "aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy",
-    "aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly",
-  ]
 }
 
 
-resource "aws_eks_node_group" "nodes" {
-  cluster_name    = "${aws_eks_cluster.CLUSTER.name}"
-  node_group_name = "node_sping_boot"
-  node_role_arn   = "${aws_iam_role.eks_nodes_role.arn}"
-  subnet_ids      = [
-      "${aws_subnet.unir_subnet_cluster_1.id}","${aws_subnet.unir_subnet_cluster_2.id}"
-  ]
-  scaling_config {
-    desired_size = 1
-    max_size     = 5
-    min_size     = 1
- }
+#resource "aws_eks_node_group" "nodes" {
+#  cluster_name    = "${aws_eks_cluster.CLUSTER.name}"
+#  node_group_name = "node_sping_boot"
+#  node_role_arn   = "${aws_iam_role.eks_nodes_role.arn}"
+#  subnet_ids      = [
+#      "${aws_subnet.unir_subnet_cluster_1.id}","${aws_subnet.unir_subnet_cluster_2.id}"
+#  ]
+#  scaling_config {
+#    desired_size = 1
+#    max_size     = 5
+#    min_size     = 1
+# }
 # instance_types is mediumt3 by default
 # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
 # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    "aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy",
-    "aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy",
-    "aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly",
-  ]
-}
+#  depends_on = [
+#    "aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy",
+#    "aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy",
+#    "aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly",
+#  ]
+#}
 
 output "eks_cluster_endpoint" {
   value = "${aws_eks_cluster.CLUSTER.endpoint}"
--- securityAndGroups.tf
+++ securityAndGroups.tf
@@ -91,27 +91,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   role       = "${aws_iam_role.eks_cluster_role.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.eks_cluster_role.name}"
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "${aws_iam_role.eks_nodes_role.name}"
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "${aws_iam_role.eks_nodes_role.name}"
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "${aws_iam_role.eks_nodes_role.name}"
-}
-
-
 resource "aws_key_pair" "mongoSSH" {
   key_name   = "mongoSHH-${var.SUFIX}"
   public_key = "${var.MONGO_SSH_KEY}"
