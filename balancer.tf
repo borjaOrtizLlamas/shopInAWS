5ffca73ba01e5268d776b47f9b3c17a07e6b03aa
borja <borja.ortizllamas1@gmail.com>
2020-06-08 00:30:10 +0200

    dsadsad

--- balancer.tf
+++ balancer.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "balancer" {
-  name  = "api-rest_balancer-${var.SUFIX}"
+  name  = "APIRestBalances${var.SUFIX}"
   security_groups    = ["${aws_security_group.api_rest_group.id}"]
   subnets = ["${aws_subnet.unir_subnet_cluster_1.id}",
              "${aws_subnet.unir_subnet_cluster_2.id}"]
