9b6fe4776cc6d7084f4b633361282b0ae92207b5
borja <borja.ortizllamas1@gmail.com>
2020-06-07 13:13:05 +0200

    service for api

--- ECS.tf
+++ ECS.tf
@@ -39,21 +39,38 @@
 #    value = "${aws_eks_cluster.CLUSTER.certificate_authority}"
 #}
 
+resource "aws_ecs_cluster" "api_rest_cluster" {
+  name = "api_rest_cluster"
+}
+
+
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
 
+
+
+
 resource "aws_ecs_task_definition" "APIRestSmallCompany" {
-  family                = "APIRestSmallCompany"
-  container_definitions = file("containers.json")
-  volume {
-    name      = "logs"
-  }
-  requires_compatibilities = ["FARGATE"]
-  memory = 2048
-  cpu = 1024
-  network_mode= "awsvpc"
+    family = "APIRestSmallCompany"
+    container_definitions = file("containers.json") #funciona
+    volume {
+        name      = "logs"
+    }
+    requires_compatibilities = ["FARGATE"]
+    memory = 2048
+    cpu = 1024
+    network_mode= "awsvpc"
     execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+}
+
+
+resource "aws_ecs_service" "service_for_api" {
+  name            = "serviceApiRest"
+  cluster         = "${aws_ecs_cluster.api_rest_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.APIRestSmallCompany.arn}"
+  desired_count   = 2
+  iam_role        = "${aws_iam_role.ecs_task_execution_role.arn}"
 }
 
 
