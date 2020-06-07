46b58e56114d198711db7189a32137e90c2adfb3
borja <borja.ortizllamas1@gmail.com>
2020-06-08 01:47:36 +0200

    dadsas

--- balancer.tf
+++ balancer.tf
@@ -27,7 +27,7 @@ resource "aws_lb_target_group" "targetForService" {
 #}
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = "${aws_lb.balancer.arn}"
-  port              = "8080"
+  port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
