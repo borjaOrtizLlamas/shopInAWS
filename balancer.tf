3fce871ceab9e1f3b8c44c6d8c7f3e40d4b6acb4
borja <borja.ortizllamas1@gmail.com>
2020-06-08 00:58:26 +0200

    dadsas

--- balancer.tf
+++ balancer.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "balancer" {
-  name  = "APIRestBalances${var.SUFIX}"
+  name  = "APIRestBalancer-${var.SUFIX}"
   security_groups    = ["${aws_security_group.api_rest_group.id}"]
   subnets = ["${aws_subnet.unir_subnet_cluster_1.id}",
              "${aws_subnet.unir_subnet_cluster_2.id}"]
@@ -7,4 +7,23 @@ resource "aws_lb" "balancer" {
     Environment = "${var.SUFIX}"
     Name = "api-rest_balancer-${var.SUFIX}"
   }
+}
+
+resource "aws_lb_target_group" "targetForService" {
+  name     = "targetForService"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.unir_shop_vpc_dev.id}"
+    tags = {
+    Name = "targetForService-${var.SUFIX}"
+    Environment = "${var.SUFIX}"
+  }
+
+}
+
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = "${aws_lb_target_group.targetForService.arn}"
+  target_id        = "${aws_ecs_service.service_for_api.id}"
+  port             = 8080
 }
