64d943fbbaecb742be9f13d99fe2f3681488a17f
borja <borja.ortizllamas1@gmail.com>
2020-06-16 01:28:38 +0200

    internet gateway

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "dns-private" {
-  name = "${var.SUFIX}.tfm.com"
+  name = "tfm.com"
 
   vpc {
     vpc_id = "${aws_vpc.unir_shop_vpc_dev.id}"
@@ -9,7 +9,7 @@ resource "aws_route53_zone" "dns-private" {
 
 resource "aws_route53_record" "mongo-record" {
   zone_id = "${aws_route53_zone.dns-private.zone_id}"
-  name    = "mongo.${var.SUFIX}.tfm.com"
+  name    = "mongo.tfm.com"
   type    = "A"
   ttl     = "300"
     depends_on = ["aws_eip.mongoEIP"]
@@ -18,7 +18,7 @@ resource "aws_route53_record" "mongo-record" {
 
 resource "aws_route53_record" "kibana-record" {
   zone_id = "${aws_route53_zone.dns-private.zone_id}"
-  name    = "kibana.${var.SUFIX}.tfm.com"
+  name    = "kibana.tfm.com"
   type    = "A"
   ttl     = "300"
   depends_on = ["aws_eip.kibanaEIP"]
