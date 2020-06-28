7883c346a1c12407b740333c6bcdfcb9866982a5
borja <borja.ortizllamas1@gmail.com>
2020-06-29 01:10:16 +0200

    cambio importante true - false

--- VPCAndRouting.tf
+++ VPCAndRouting.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "unir_subnet_aplications" {
   cidr_block = "${var.SUBNET_CIDR_APLICATIONS}"
   availability_zone = "${var.ZONE_SUB}"
   depends_on = ["aws_internet_gateway.unir_gat_shop"]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags = {
     Name = "UNIR-SUBNET-APLICATIONS-${var.SUFIX}"
     Environment = "${var.SUFIX}"
@@ -34,7 +34,7 @@ resource "aws_subnet" "unir_subnet_cluster_1" {
   vpc_id = "${aws_vpc.unir_shop_vpc_dev.id}"
   cidr_block = "${var.SUBNET_CIDR_CLUSTER_1}"
   availability_zone = "${var.ZONE_SUB_CLUSTER_1}"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
   tags = {
     "kubernetes.io/cluster/UNIR-API-REST-CLUSTER-${var.SUFIX}" = "shared"
     Name = "UNIR-SUBNET-CLUSTER-1-${var.SUFIX}"
@@ -47,7 +47,7 @@ resource "aws_subnet" "unir_subnet_cluster_2" {
   vpc_id = "${aws_vpc.unir_shop_vpc_dev.id}"
   cidr_block = "${var.SUBNET_CIDR_CLUSTER_2}"
   availability_zone = "${var.ZONE_SUB_CLUSTER_2}"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
   tags = {
     "kubernetes.io/cluster/UNIR-API-REST-CLUSTER-${var.SUFIX}" = "shared"
     Name = "UNIR-SUBNET-CLUSTER-2-${var.SUFIX}"
